ed9260598004bfa79fcdf912b4a0ae14ece810dc
Antoine Cœur <coeur@gmx.fr>
2015-06-08 21:57:37 +0200

    spelling fix

--- NEW WEBSITE/schedule/php/setSignupData.php
+++ NEW WEBSITE/schedule/php/setSignupData.php
@@ -7,10 +7,10 @@
 	#cemma-usc.net/schedule/php/setSignupData.php?usedby=dh&instr=JEOL JIB-4500 - FIB SEM&date=2012-02-03&slot=4
 	#$sql = "SELECT UserName FROM user WHERE ActiveUser='active' ORDER BY UserName";
 	$sql = "UPDATE schedule SET Usedby='$usedby' WHERE InstrumentName='$instr_name' AND Date='$date' AND Slot='$slot' AND Status='1'"	;
-	$result=mysql_query($sql) or die( "An error has ocured in query1: " .mysql_error (). ":" .mysql_errno ()); 
+	$result=mysql_query($sql) or die( "An error has occurred in query1: " .mysql_error (). ":" .mysql_errno ()); 
 		
 	$sql = "SELECT Usedby FROM schedule WHERE InstrumentName='$instr_name' AND Date='$date' AND Slot='$slot' AND Status='1'";
-	$result=mysql_query($sql) or die( "An error has ocured in query2: " .mysql_error (). ":" .mysql_errno ()); 
+	$result=mysql_query($sql) or die( "An error has occurred in query2: " .mysql_error (). ":" .mysql_errno ()); 
 	$row = mysql_fetch_array($result);
 	
 	$output = "";
